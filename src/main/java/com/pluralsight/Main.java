edb49ef8c6cd12c1f672a952610d316cd82d151b
Alexis Barrett <happynessb09@gmail.com>
2026-05-11 13:24:51 -0400

    Started the project

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(){
+
+        //creating the moped
+        Moped m = new Moped();
+
+        m.setColor("Gray and White");
+        m.setFuelCapacity(3);
+        m.setCargoCapacity(2);
+        m.setNumberOfPassengers(1);
+
+        //Creating the car
+        Car Lexus = new Car();
+
+        Lexus.setColor("Blue");
+        Lexus.setCargoCapacity(6);
+        Lexus.setFuelCapacity(25);
+        Lexus.setNumberOfPassengers(4);
+
+        SemiTruck s = new SemiTruck();
+
+        
+
+
+
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+    private String name;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+}
